a8460b3af3bcf18ac1b2004c7e898cafaa144cb7
crefsgaard <c.b.refsgaard2@newcastle.ac.uk>
2022-03-17 17:49:49 +0000

    Navigation bar implementation

--- app/src/main/java/com/example/csc8019_teamprojectnotemplate/MainActivity.java
+++ app/src/main/java/com/example/csc8019_teamprojectnotemplate/MainActivity.java
@@ -1,25 +1,66 @@
 package com.example.csc8019_teamprojectnotemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Here is a click msg", Toast.LENGTH_SHORT).show();
-            }
-        });
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.homeFragment);
+
+    }
+
+    //Inspiration for navigation bar: https://www.geeksforgeeks.org/bottom-navigation-bar-in-android/
+    HomeFragment homeFragment = new HomeFragment();
+    BookingFragment bookingFragment = new BookingFragment();
+    RouteFragment routeFragment = new RouteFragment();
+    RestaurantsFragment restaurantsFragment = new RestaurantsFragment();
+    AttractionsFragment attractionsFragment = new AttractionsFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.homeFragment) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
+            return true;
+        }
+
+        else if (item.getItemId() == R.id.bookingFragment) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, bookingFragment).commit();
+            return true;
+        }
+
+        else if (item.getItemId() == R.id.routeFragment) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, routeFragment).commit();
+            return true;
+        }
+
+        else if (item.getItemId() == R.id.restaurantsFragment) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, restaurantsFragment).commit();
+            return true;
+        }
+
+        else if (item.getItemId() == R.id.attractionsFragment) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, attractionsFragment).commit();
+            return true;
+        }
+
+        else {
+            return false;
+        }
     }
 }
