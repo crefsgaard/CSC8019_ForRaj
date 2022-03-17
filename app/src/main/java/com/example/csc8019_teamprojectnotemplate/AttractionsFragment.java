9049dac5c4b10d494911ab841dbf06b272d04e89
crefsgaard <c.b.refsgaard2@newcastle.ac.uk>
2022-03-17 17:50:59 +0000

    Navigation bar implementation

---
+++ app/src/main/java/com/example/csc8019_teamprojectnotemplate/AttractionsFragment.java
@@ -0,0 +1,20 @@
+package com.example.csc8019_teamprojectnotemplate;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class AttractionsFragment extends Fragment {
+
+    public AttractionsFragment(){
+        // require a empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_attractions, container, false);
+    }
+}
